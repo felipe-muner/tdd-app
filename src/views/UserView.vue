012f1e0019b385635ac3487c9e0ef056f48e7577
felipemuner <felipe.muner@gmail.com>
2020-02-17 13:10:03 +0700

    test to check component snapshot

---
+++ src/views/UserView.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: "UserView"
+};
+</script>
+
+<template>
+  <div>
+    Hello World
+  </div>
+</template>
--- tests/unit/UserView.spec.js
+++ tests/unit/UserView.spec.js
@@ -1,3 +1,12 @@
-describe('UserView', () => {
-  it('works', () => {})
-})
+import { shallowMount } from "@vue/test-utils";
+import UserView from "@/views/UserView";
+
+describe("UserView", () => {
+  it("renders the component", () => {
+    // arrange
+    const wrapper = shallowMount(UserView);
+
+    // assert
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+});
